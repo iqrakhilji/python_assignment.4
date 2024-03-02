2bb23ecd1949639ea548c45aa780a20995f2d8a3
iqrakhilji <143201499+iqrakhilji@users.noreply.github.com>
2024-03-02 17:02:33 +0530

    Add files via upload

---
+++ question_1.py
@@ -0,0 +1,11 @@
+# Define a function 'personal_details'.
+def personal_details():
+    # Define variables 'name' and 'age' and assign values to them.
+    name, age = "iqra", 21
+    # Define a variable 'address' and assign a value to it.
+    address = "Aurangabad, Maharashtra, India"
+    # Print the personal details using string formatting.
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+# Call the 'personal_details' function to display the details.
+personal_details()
